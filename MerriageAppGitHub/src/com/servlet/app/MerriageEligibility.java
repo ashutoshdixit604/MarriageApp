97823e20118e489f95e70138e8ed9fad8e3c7eda
Amar <Amar@DESKTOP-HSI3HNB>
2019-04-01 00:16:34 +0530

    Final commit

--- MerriageAppGitHub/src/com/servlet/app/MerriageEligibility.java
+++ MerriageAppGitHub/src/com/servlet/app/MerriageEligibility.java
@@ -1,6 +1,7 @@
 package com.servlet.app;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -8,15 +9,50 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class MerriageEligibility extends HttpServlet {
-   @Override
-protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-	// TODO Auto-generated method stub
-	super.doGet(req, resp);
-} 
-   
-   @Override
-	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		super.doPost(req, resp);
-	}
+	@Override
+    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+      PrintWriter pw = null;
+      String tname = null;
+      String tage = null;
+      String tgender=null;
+      int age = 0;
+      
+      //create print writer object
+      pw = resp.getWriter();
+      
+      resp.setContentType("text/html");
+      
+       try {
+      //get value from request object
+       tname = req.getParameter("pname");
+       tage = req.getParameter("page");
+       age = Integer.parseInt(tage);
+       }
+       catch(Exception e){
+    	   pw.println("<h1 style = 'color:green; text-align:center'>Name and Age is Required</h1>");
+       }
+    
+       
+    
+           if(req.getParameter("gender").equalsIgnoreCase("MALE"))
+       	   tgender = "male";
+           else
+    	   tgender = "female";
+ 
+     if(tname!=null && age!=0) {    
+         if(tgender=="female" && age>=18) {
+    	     pw.println("<br><h1 style='color'red; text-align:center'>Miss "+tname+" you are eligible for marriage</h1>"); 
+          }
+           else if(tgender=="male" && age>=21) {
+    	   pw.println("<br><h1 style='color'red; text-align:center'>Mr."+tname+" you are eligible for marriage</h1>");
+       }  	   
+        else {
+    	   pw.println("<br><h1 style='color'green; text-align:center'>Mr/Miss/Mrs."+tname+" you are not eligible for voting</h1>");
+       }
+         
+         pw.println("<br><a href ='input.html'><image src='home.jpg' height='10%' width='10%'></a>");
+   }//if
+      
+       pw.close();
+   } 
 }

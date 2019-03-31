d12d6d537f0c038b4a1f8aa3025f3d0f316df298
Amar <Amar@DESKTOP-HSI3HNB>
2019-04-01 00:32:07 +0530

    servlet class commit

--- MerriageAppGitHub/src/com/servlet/app/MerriageEligibility.java
+++ MerriageAppGitHub/src/com/servlet/app/MerriageEligibility.java
@@ -9,6 +9,11 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class MerriageEligibility extends HttpServlet {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	@Override
     public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
       PrintWriter pw = null;
@@ -52,5 +57,10 @@ public class MerriageEligibility extends HttpServlet {
    }//if
       
        pw.close();
-   } 
-}
+   }//get(-)
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+                doGet(req,resp);
+	}
+}//class
